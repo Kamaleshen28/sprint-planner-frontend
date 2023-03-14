67f77fbd412f396aa8457e00ac9df0139010d7d8
Harbir Singh <harbir066@gmail.com>
2023-03-14 18:17:51 +0530

    feat : add startDateInput component

--- src/Components/StartDateInput/index.jsx
+++ src/Components/StartDateInput/index.jsx
@@ -5,15 +5,6 @@ import PropTypes from 'prop-types';
 
 export default function StartDateInput({ value, setValue }) {
   return (
-    // <div style={{ width: '300px' }}>
-    //   <label htmlFor="startDate">Start Date</label>
-    //   <input
-    //     id="startDate"
-    //     type="date"
-    //     value={value}
-    //     onChange={(event) => setValue(event.target.value)}
-    //   />
-    // </div>
     <div style={{ marginLeft: '30px' }}>
       <InputLabel htmlFor="startDate">
         <span
