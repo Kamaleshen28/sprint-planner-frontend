d2132b69a35bcccc28dbcbee716e0323791e4477
prajith aarya <prajith.sonu@gmail.com>
2023-03-15 14:54:39 +0530

    feat: integrate gantt chart

--- src/Assets/dataMapper.js
+++ src/Assets/dataMapper.js
@@ -1,45 +1,95 @@
 import * as response from './sampleData.json';
-const { sprints, title: projectTitle } = response;
+// const { sprints, title: projectTitle, sprintDuration } = response;
 const tempDate = new Date('2021-01-04');
 const projectStartDate = tempDate.getTime();
-// import { v4 } from 'uuid';
+import { DataContext } from '../Contexts/DataContext';
+import { useContext } from 'react';
 
+// import { v4 } from 'uuid';
+const dateDuration = (sprintDuration) => {
+  const milliseconds = projectStartDate; // Convert Unix timestamp to milliseconds
+  const twoWeeksMs = 7 * sprintDuration * 24 * 60 * 60 * 1000; // Calculate two weeks in milliseconds
+  return milliseconds + twoWeeksMs; // Add two weeks and convert back to Unix timestamp
+};
 const getGanttChartFormatData = () => {
+  const { apiResponse, sprints } = useContext(DataContext);
+
   const toReturn = []; // 1-D array
   let storyYAxis = 0;
-  //   const storyCharMap = {};
-  //   sprints.forEach((sprint) => {
-  //     sprint.forEach((story) => {
-  //       let { id } = story;
-  //       if (!storyCharMap[id]) {
-  //         storyCharMap[id] = v4();
-  //       }
-  //     });
-  //   });
+
   sprints.forEach((sprint) => {
     sprint.forEach((story) => {
-      let { id, dependencies, developers, title, startDay, endDay } = story;
+      let {
+        id,
+        dependencies,
+        developers,
+        title,
+        startDay,
+        endDay,
+        storyPoints,
+      } = story;
+      var startStory = projectStartDate + startDay * 60 * 60 * 24 * 1000; //timestamp
+      var endStory = projectStartDate + endDay * 60 * 60 * 24 * 1000;
+      const startdate = new Date(startStory); // Convert Unix timestamp to JavaScript Date object
+      const dayOfstart = startdate.getUTCDay();
+      const enddate = new Date(endStory); // Convert Unix timestamp to JavaScript Date object
+      const dayOfend = enddate.getUTCDay();
+      if (dayOfstart === 0) {
+        //sunday
+        const date = new Date(startStory);
+
+        // Add one day to the date
+        date.setDate(date.getDate() + 1);
+        startStory = date.getTime();
+      }
+      if (dayOfstart === 6) {
+        const date = new Date(startStory);
+        date.setDate(date.getDate() + 2);
+        startStory = date.getTime();
+      }
+      if (dayOfend === 0) {
+        const date = new Date(endStory);
+        date.setDate(date.getDate() + 1);
+        endStory = date.getTime();
+      }
+      if (dayOfend === 6) {
+        const date = new Date(endStory);
+        date.setDate(date.getDate() + 2);
+        endStory = date.getTime();
+      }
+
       let storyToAdd = {
         id: id.toString(),
         dependency: dependencies.map((id) => id.toString()),
         developer: developers[0].name,
         name: title,
-        start: projectStartDate + startDay * 60 * 60 * 24 * 1000,
-        end: projectStartDate + endDay * 60 * 60 * 24 * 1000,
+
+        start: startStory,
+        end: endStory,
+        // start: startOfWeek.getTime(),
+        // end: endOfWeek.getTime(),
+        storyPoints: storyPoints,
+        duration: endDay - startDay,
+
         y: storyYAxis,
       };
       toReturn.push(storyToAdd);
+
       ++storyYAxis;
     });
   });
   const formattedData = {
     title: {
-      text: projectTitle,
+      text: 'sprint planner',
     },
-    colors: ['#2f7ed8', '#0d233a', '#8bbc21'],
+    colors: ['#05445e', '#189ab4', '#75e6da', '#d4f1f4'],
+    sprintDuration: [
+      projectStartDate,
+      dateDuration(apiResponse.sprintDuration),
+    ],
     series: [
       {
-        name: projectTitle,
+        name: 'sprint planner',
         data: toReturn,
       },
     ],
@@ -48,3 +98,51 @@ const getGanttChartFormatData = () => {
   return formattedData;
 };
 export default getGanttChartFormatData;
+
+// var weekends = [];
+// var storyStartDate = projectStartDate + startDay * 60 * 60 * 24 * 1000;
+
+// var storyEndDate = projectStartDate + endDay * 60 * 60 * 24 * 1000;
+// for (
+//   let i = storyStartDate;
+//   i <= storyEndDate;
+//   i += 24 * 60 * 60 * 1000
+// ) {
+//   const date = new Date(i); // Convert Unix timestamp to JavaScript Date object
+//   const dayOfWeek = date.getUTCDay(); // Get day of the week (0=Sunday, 1=Monday, etc.)
+
+//   // If the day is a Saturday or Sunday, add it to the weekends array
+//   if (dayOfWeek === 0) {
+//     weekends.push(date);
+//   }
+//   if (dayOfWeek === 6) {
+//     weekends.push(date);
+//   }
+// }
+
+// if (weekends.length > 1) {
+//   var i = 0;
+//   while (i < weekends.length) {
+//     if (weekends[i].getUTCDay() - weekends[i + 1].getUTCDay() === 6) {
+//       let storyToAdd = {
+//         id: id.toString(),
+//         dependency: dependencies.map((id) => id.toString()),
+//         developer: developers[0].name,
+//         name: title,
+
+//         start: projectStartDate + startDay * 60 * 60 * 24 * 1000,
+//         end: projectStartDate + endDay * 60 * 60 * 24 * 1000,
+//         // start: startOfWeek.getTime(),
+//         // end: endOfWeek.getTime(),
+//         storyPoints: storyPoints,
+//         duration: endDay - startDay,
+
+//         y: storyYAxis,
+//       };
+//       toReturn.push(storyToAdd);
+//       //stroyAxis dont incremnet
+
+//       i += 2;
+//     }
+//   }
+// }
--- src/Pages/GanttChart/index.jsx
+++ src/Pages/GanttChart/index.jsx
@@ -5,43 +5,118 @@ import getGanttChartFormatData from '../../Assets/dataMapper';
 
 const GanttChart = () => {
   const data = getGanttChartFormatData();
+  var plots = [];
+  const startDate = data.sprintDuration[0];
+  const endDate = data.sprintDuration[1];
+  const weekends = [];
+
+  // Loop through each day between the start and end timestamps
+  for (let i = startDate; i <= endDate; i += 24 * 60 * 60 * 1000) {
+    const date = new Date(i); // Convert Unix timestamp to JavaScript Date object
+    const dayOfWeek = date.getUTCDay(); // Get day of the week (0=Sunday, 1=Monday, etc.)
+
+    // If the day is a Saturday or Sunday, add it to the weekends array
+    if (dayOfWeek === 0) {
+      date.setUTCDate(date.getUTCDate() + 1);
+      weekends.push(date);
+    }
+    if (dayOfWeek === 6) {
+      weekends.push(date);
+    }
+  }
+
+  // console.log(new Date(saturdays * 1000), new Dasundays());
+
+  for (var i = 0; i < weekends.length; i += 2) {
+    // var start = new Date(data.series[0].data[i].start);
+    // var end = new Date(data.series[0].data[i].end);
+
+    plots.push({
+      color: '#f2f2f2',
+      from: weekends[i], // Saturday
+      to: weekends[i + 1],
+      // Sunday
+    });
+  }
+
   const options = {
+    plotOptions: {
+      series: {
+        tooltip: {
+          headerFormat: '<b>{point.key}</b><br>',
+          pointFormat:
+            'Story Points: {point.storyPoints}<br>' +
+            'Start: {point.start:%e %b %Y}<br>' +
+            'End: {point.end:%e %b %Y}<br>' +
+            'Duration: {point.duration} day(s)',
+        },
+      },
+    },
+    chart: {
+      scrollablePlotArea: {
+        minWidth: 2000,
+
+        scrollPositionX: 1,
+        scrollbar: {
+          enabled: true,
+          height: 20,
+        },
+      },
+      marginRight: 20,
+    },
+
     yAxis: {
       type: 'category',
+      labels: {
+        style: {
+          width: '900px',
+          textOverflow: 'ellipsis',
+        },
+      },
+
       grid: {
-        borderColor: '#3a5d96',
+        //background color for highcharts
         columns: [
           {
             title: {
               text: 'Stories',
-              rotation: 45,
+              style: {
+                color: '#000',
+                fontWeight: 'bold',
+                fontSize: '15px',
+              },
+
+              rotation: 0,
               y: -15,
               x: -15,
+            },
+            labels: {
+              format: '{point.name}',
+              style: {
+                color: '#000',
+              },
             },
           },
           {
             title: {
-              text: 'Developer',
-              rotation: 45,
-              y: -15,
-              x: -15,
+              text: 'Owner',
+
+              style: {
+                color: '#000',
+                fontWeight: 'bold',
+                fontSize: '15px',
+              },
+              rotation: 0,
+              y: -13,
+              x: -1,
             },
+
             labels: {
               format: '{point.developer}',
-            },
-          },
-          {
-            title: {
-              text: 'Duration',
-              rotation: 45,
-              y: -15,
-              x: -15,
-            },
-            labels: {
-              formatter: function () {
-                var days = 1000 * 60 * 60 * 24;
-                var number = (this.point.end - this.point.start) / days;
-                return Math.round(number * 100) / 100;
+              useHTML: true,
+              style: {
+                color: '#000',
+                width: '200px',
               },
             },
           },
@@ -49,28 +124,65 @@ const GanttChart = () => {
       },
     },
     xAxis: [
+      // {
+      //   type: 'datetime',
+      //   tickPositioner: function () {
+      //     var ticks = [],
+      //       tick = this.dataMin;
+
+      //     // Loop through each day from dataMin to dataMax
+      //     for (tick; tick <= this.dataMax; tick += 24 * 3600 * 1000) {
+      //       // Check if the day is not a weekend (Saturday or Sunday)
+      //       // var day = new Date(tick).getUTCDay();
+      //       // if (day == 0) {
+      //       //   ticks.push(tick + 7);
+      //       //   continue;
+      //       // }
+      //       ticks.push(tick);
+      //     }
+
+      //     return ticks;
+      //   },
+      //   tickInterval: 1000 * 60 * 60 * 24, // day
+      //   labels: {
+      //     format: '{value: %a, %e. %b}',
+      //     style: {
+      //       color: '#000',
+      //     }, // Day label format
+      //   },
+      // },
       {
-        labels: {
-          format: '{value:%w}', // day of the week
-        },
-        grid: {
-          // default setting
-          enabled: true,
-        },
-        tickInterval: 1000 * 60 * 60 * 24, // Day
+        breaks: [
+          {
+            from: Date.UTC(2021, 0, 9),
+            to: Date.UTC(2021, 0, 11),
+            breakSize: 0,
+          },
+          {
+            from: Date.UTC(2021, 0, 16),
+            to: Date.UTC(2021, 0, 18),
+            breakSize: 0,
+          },
+        ],
       },
+
       {
+        type: 'datetime',
         labels: {
           format: '{value:Week %W}',
+          style: {
+            color: '#000',
+          },
         },
         tickInterval: 1000 * 60 * 60 * 24 * 7, // week
       },
     ],
+
     ...data,
   };
 
   return (
-    <div style={{ minHeight: '90vh' }}>
+    <div style={{ minHeight: '100vh' }}>
       <HighchartsReact
         highcharts={Highcharts}
         constructorType={'ganttChart'}
