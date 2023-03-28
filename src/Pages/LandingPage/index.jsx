b7fc7eb46a4a9a223b06130d7f5fc0b6aaa21dc9
Smita <Smita_Jha@mckinsey.com>
2023-03-28 17:52:59 +0530

    feat: drop-down for selecting project by status

--- src/Pages/LandingPage/index.jsx
+++ src/Pages/LandingPage/index.jsx
@@ -4,9 +4,10 @@ import Box from '@mui/material/Box';
 import { DataContext } from '../../Contexts/DataContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { ProjectCard, Header } from '../../Components';
@@ -19,15 +20,8 @@ export default function LandingPage() {
   const [projects, setProjects] = React.useState([]);
   const [filterType, setFilterType] = React.useState('');
   const { setProjectId, projectId } = React.useContext(DataContext);
-  const handleFilterType = (currentfilter) => {
-    setFilterType(currentfilter);
-  };
-  const [open, setOpen] = React.useState(false);
   const [filteredProjects, setFilteredProjects] = React.useState(projects);
   const [query, setQuery] = React.useState('');
-  const handleToggle = () => {
-    setOpen(!open);
-  };
   React.useEffect(() => {
     axios
       .get('http://localhost:8080/api/projects', {
@@ -36,7 +30,6 @@ export default function LandingPage() {
       .then((res) => {
         setProjects(res.data.data);
         setFilteredProjects(res.data.data);
-        console.log(res);
       })
       .catch((error) => {
         console.log(error);
@@ -52,15 +45,16 @@ export default function LandingPage() {
     let results = projects.filter((project) =>
       project.title.toLowerCase().includes(query),
     );
-    if (filterType !== '') {
+    if (filterType !== '' && filterType !== 'All') {
       results = results.filter((project) => {
         return project.status === filterType;
       });
+      setFilteredProjects(results);
+    } else {
+      setFilteredProjects(results);
     }
-    setFilteredProjects(results);
   }, [query, filterType]);
   const handleClick = (id) => {
-    console.log('clicked', id);
     setProjectId(id);
     localStorage.setItem('projectId', id);
     const selectedProject = filteredProjects.find(
@@ -73,6 +67,11 @@ export default function LandingPage() {
       navigate('/edit');
     }
   };
+  const handleChange = (event) => {
+    setFilterType(
+      event.target.value === 'draft' ? 'unsupportedInput' : event.target.value,
+    );
+  };
   return (
     <>
       <Header />
@@ -83,7 +82,7 @@ export default function LandingPage() {
             <button className="btn-search">
               <FontAwesomeIcon
                 icon={faSearch}
-                style={{ marginTop: '0px', fontSize: '25px' }}
+                style={{ marginTop: '0px', fontSize: '20px' }}
               />
             </button>
             <input
@@ -94,74 +93,59 @@ export default function LandingPage() {
             />
           </div>
           <div className="filter-search">
-            {open ? (
-              <KeyboardArrowUpIcon onClick={() => handleToggle()} />
-            ) : (
-              <KeyboardArrowDownIcon onClick={() => handleToggle()} />
-            )}
-            <div>FILTER</div>
-            <FilterAltIcon />
+            <FormControl
+              sx={{ m: 1, width: 300 }}
+              style={{ backgroundColor: 'white', borderRadius: '10px' }}
+            >
+              <InputLabel
+                variant="standard"
+                htmlFor="uncontrolled-native"
+                sx={{ mb: 1.4, ml: 2 }}
+              >
+                Select Status
+              </InputLabel>
+              <Select
+                labelId="demo-select-small"
+                id="demo-select-small"
+                value={filterType === 'unsupportedInput' ? 'draft' : filterType}
+                label="Select Status"
+                onChange={handleChange}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={'All'}>All</MenuItem>
+                <MenuItem value={'planned'}>Planned</MenuItem>
+                <MenuItem value={'draft'}>Draft</MenuItem>
+              </Select>
+            </FormControl>
           </div>
         </div>
-        {open && (
-          <div className="status-radio-btn">
-            <div className="radio-btn">
-              Planned
-              <input
-                type="radio"
-                value={filterType}
-                name="filterType"
-                checked={filterType === 'planned'}
-                onClick={() => handleFilterType('planned')}
-              />
-            </div>
-            <div className="radio-btn">
-              Draft
-              <input
-                className="radio-btn-right"
-                type="radio"
-                value={filterType}
-                name="filterType"
-                checked={filterType === 'unsupportedInput'}
-                onClick={() => handleFilterType('unsupportedInput')}
-              />
-            </div>
-            <div>
-              {filterType !== '' && (
-                <div className="radio-btn">
-                  Clear All
-                  <input
-                    className="radio-btn-right"
-                    type="radio"
-                    value={filterType}
-                    name="filterType"
-                    checked={filterType === ''}
-                    onClick={() => handleFilterType('')}
-                  />
-                </div>
-              )}
-            </div>
-          </div>
-        )}
+
         {filteredProjects.length === 0 ? (
           <div>No Projects</div>
         ) : (
           <Box sx={{ flexGrow: 1, width: '80%', margin: 'auto' }}>
             <Grid container rowSpacing={2} sx={{ mt: 2 }}>
-              {filteredProjects.map((project) => (
-                <Grid
-                  key={project.id}
-                  onClick={() => handleClick(project.id)}
-                  xs={12}
-                  md={4}
-                  sx={{ mt: 2 }}
-                >
-                  <ProjectCard
-                    project={project}
-                    // onClick={() => handleClick(project.id)}
-                  />
-                </Grid>
-              ))}
+              {filteredProjects.map(
+                (project) => (
+                  console.log(project),
+                  (
+                    <Grid
+                      key={project.id}
+                      onClick={() => handleClick(project.id)}
+                      xs={12}
+                      md={4}
+                      sx={{ mt: 2 }}
+                    >
+                      <ProjectCard
+                        project={project}
+                        // onClick={() => handleClick(project.id)}
+                      />
+                    </Grid>
+                  )
+                ),
+              )}
             </Grid>
           </Box>
         )}
